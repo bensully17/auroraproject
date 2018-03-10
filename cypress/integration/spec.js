21111a4281080ac022a74312e6bd668eb48b206e
Bensully17 <bensully513@gmail.com>
2018-03-10 10:50:28 -0700

    updated E2E tests

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,10 +1,15 @@
 
-describe('Kitchen Sink', function () {
-  it('.should() - assert that <title> is correct', function () {
+describe('Aurora Tests', function () {
+  it('.should() - have functional API calls', function () {
     
-    cy.visit('https://example.cypress.io')
-
-    cy.title().should('include', 'Kitchen Sink')
-    
+    cy.visit('http://bensgalvanizeprojects.com/auroraforecast/index.html')
+    cy.title().should('include', 'Aurora')
+    cy.get('#options').find('button').should('contain', 'Arriving at Earth')
+    cy.get('#buttons').find('button').should('contain', 'Hemisphere')
+    cy.get('#speed').find('.value').should('exist')
+    cy.get('#density').find('.value').should('exist')
+    cy.get('#bz').find('.value').should('exist')
+    cy.get('#intensity').find('.value').should('exist')
+    cy.get('#ovation').find('img').should('have.attr', 'src').and('include', 'http://services.swpc.noaa.gov/images/animations/ovation-north/latest.jpg')
   })
 })
